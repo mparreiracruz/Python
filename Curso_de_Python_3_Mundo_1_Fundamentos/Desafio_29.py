f5e51d835c1fd453ff1dfc17a35fadcd2d6662df
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-06-26 15:22:22 -0300

    Update Desafio_29.py

--- Curso_de_Python_3_Mundo_1_Fundamentos/Desafio_29.py
+++ Curso_de_Python_3_Mundo_1_Fundamentos/Desafio_29.py
@@ -3,7 +3,13 @@ Desafio 029
 
 Escreva um programa que leia a velocidade de um carro.
 Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
-
 A multa vai custar R$7,00 por cada Km acima do limite.
-
 '''
+
+velocidade_carro = float(input('Digite a velocidade do seu carro: '))
+
+if velocidade_carro > 80:
+    multa = (velocidade_carro - 80) * 7
+    print('Voce foi multado no valor de R${}0.'.format(multa))
+else:
+    print('Voce está na velocidade permitida.')
