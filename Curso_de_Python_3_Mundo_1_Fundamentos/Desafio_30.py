6ed4cd43b251d6f66c99cd47c71fe5756f519387
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-06-26 15:22:46 -0300

    Update Desafio_30.py

--- Curso_de_Python_3_Mundo_1_Fundamentos/Desafio_30.py
+++ Curso_de_Python_3_Mundo_1_Fundamentos/Desafio_30.py
@@ -4,3 +4,10 @@ Desafio 030
 Crie um programa que leia um número inteiro e mostre na tela
 se ele é PAR ou ÍMPAR.
 '''
+
+n = int(input('Digite um número inteiro: '))
+
+if n % 2 == 0:
+    print('PAR')
+else:
+    print('ÍMPAR')
