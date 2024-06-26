b6c3b150ca45d43de78fed504da0d4aadf46a2da
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-06-26 15:23:47 -0300

    Update Desafio_33.py

--- Curso_de_Python_3_Mundo_1_Fundamentos/Desafio_33.py
+++ Curso_de_Python_3_Mundo_1_Fundamentos/Desafio_33.py
@@ -4,3 +4,7 @@ Desafio 033
 Faça um programa que leia tres números e mostre
 qual é o maior e qual é o menor.
 '''
+
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+n3 = float(input('Digite o terceiro número: '))
