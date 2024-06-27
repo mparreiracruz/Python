93248d050f66c18102d40398155e6b7d799c5f27
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-06-27 11:58:38 -0300

    Update Desafio_28.py

--- Curso_de_Python_3_Mundo_1_Fundamentos/Desafio_28.py
+++ Curso_de_Python_3_Mundo_1_Fundamentos/Desafio_28.py
@@ -1,4 +1,6 @@
-import random
+from random import randint
+from time import sleep
+
 '''
 Desafio 028
 
@@ -10,13 +12,27 @@ O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
 '''
 
-
+'''
 n = int(input('Digite um número inteiro entre 0 e 5 que pode ter sido escolhido pela máquina: '))
 
-num = random.randint(0, 5)
+num = randint(0, 5)
 print(num)
 
 if n == num:
     print('Parabéns, voce acertou!')
 else:
     print('Voce perdeu.')
+'''
+
+computador = randint(0, 5)
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+print('PROCESSANDO...')
+sleep(3)
+
+jogador = int(input('Em que número eu pensei ? '))
+if jogador == computador:
+    print('Parabéns! Voce conseguir me vencer !')
+else:
+    print('Ganhei! Eu pensei no número {} e não no {}!'.format(computador, jogador))
